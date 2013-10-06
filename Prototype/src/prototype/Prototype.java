fa7aee5a9aa6b6f96d5e396c96b8fd2c30585c73
juan2ramos <juan2ramos@gmail.com>
2013-10-05 21:13:20 -0500

    setVisible Form

--- Prototype/src/prototype/Prototype.java
+++ Prototype/src/prototype/Prototype.java
@@ -14,6 +14,7 @@ public class Prototype {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //System.out.print("kjkj");
+        new CuentaManager().setVisible(true);
     }
 }
